0ac66ad670e33e148ffbaeca5c6bc906372509a8
Fady Raafat <fady.raafat@btech.com>
2023-08-02 11:57:32 +0300

    Initial commit

---
+++ sdkPOC/src/main/java/com/example/sdkpoc/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.sdkpoc
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+}
